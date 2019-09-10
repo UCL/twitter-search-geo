9c891012f38fd8b776de866eb67bcfc67f3b38da
David Guzman <d.guzman@ucl.ac.uk>
2019-09-10 17:02:18 +0100

    Add entity to retrieve schedules from database

---
+++ twitter-search-geo-ejb/src/main/java/uk/ac/ucl/twitter/search/geo/ScheduleAppLocationEntity.java
@@ -0,0 +1,73 @@
+package uk.ac.ucl.twitter.search.geo;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.NamedQuery;
+
+/**
+ * Entity class containing schedule information for API calls based on location.
+ *
+ * @author David Guzman {@literal d.guzman at ucl.ac.uk}
+ * @since 1.0
+ */
+@Entity
+@NamedQuery(
+  name="ScheduleAppLocationEntity.findAll",
+  query="SELECT l FROM ScheduleAppLocationEntity l"
+)
+public class ScheduleAppLocationEntity {
+
+  /**
+   * The name of the application, used as authentication context.
+   */
+  private String appName;
+
+  /**
+   * The schedule for an API call in CRON format.
+   */
+  private int intervalMinutes;
+
+  /**
+   * The name of the geographical location. To be used as identifier for
+   * updates.
+   */
+  @Enumerated(EnumType.STRING)
+  private Location location;
+
+  /**
+   * Named query to find ScheduleAppLocationEntity.
+   */
+  public static final String QUERY_FIND_ALL =
+    "ScheduleAppLocationEntity.findAll";
+
+  /**
+   * No args constructor for JPA.
+   */
+  public ScheduleAppLocationEntity() { }
+
+  /**
+   * Getter for the appName field.
+   * @return The name of the application
+   */
+  public String getAppName() {
+    return appName;
+  }
+
+  /**
+   * Getter for the cronSchedule field.
+   * @return A String representing the schedule in CRON format
+   */
+  public int getIntervalMinutes() {
+    return intervalMinutes;
+  }
+
+  /**
+   * Getter for the geographical location.
+   * @return The name of the geographical location
+   */
+  public Location getLocation() {
+    return location;
+  }
+
+}
---
+++ twitter-search-geo-ejb/src/test/java/uk/ac/ucl/twitter/search/geo/ScheduleAppLocationEntityTest.java
@@ -0,0 +1,60 @@
+package uk.ac.ucl.twitter.search.geo;
+
+import org.junit.jupiter.api.Test;
+
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+
+import java.lang.reflect.Field;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+/**
+ * Unit tests for {@see uk.ac.ucl.twitter.search.geo.ScheduleAppLocationEntity}
+ * based on the recommendations listed in
+ * <a href="http://mjremijan.blogspot.com/2016/06/unit-testing-jpa-annotationsstop_20.html">
+ * http://mjremijan.blogspot.com/2016/06/unit-testing-jpa-annotationsstop_20.html
+ * </a>
+ *
+ * @author David Guzman {@literal d.guzman at ucl.ac.uk}
+ */
+public class ScheduleAppLocationEntityTest {
+
+  @Test
+  public void hasEntityAnnotation() {
+    Class<ScheduleAppLocationEntity> clEntity = ScheduleAppLocationEntity.class;
+    Entity annotation = clEntity.getAnnotation(Entity.class);
+    assertNotNull(annotation, "Expect ScheduleAppLocationEntity to be annotated as Entity");
+    assertEquals("", annotation.name(), "Expect name to be empty");
+  }
+
+  @Test
+  public void hasRequiredFields() {
+    Class<ScheduleAppLocationEntity> clEntity = ScheduleAppLocationEntity.class;
+    assertAll("Expect ScheduleAppLocationEntity to have all required fields",
+      () -> assertNotNull(clEntity.getDeclaredField("appName")),
+      () -> assertNotNull(clEntity.getDeclaredField("intervalMinutes")),
+      () -> assertNotNull(clEntity.getDeclaredField("location"))
+    );
+  }
+
+  @Test
+  public void hasRequiredMethods() {
+    Class<ScheduleAppLocationEntity> clEntity = ScheduleAppLocationEntity.class;
+    assertAll("Expect ScheduleAppLocationEntity to have all required methods",
+      () -> assertNotNull(clEntity.getDeclaredMethod("getLocation")),
+      () -> assertNotNull(clEntity.getDeclaredMethod("getIntervalMinutes")),
+      () -> assertNotNull(clEntity.getDeclaredMethod("getAppName"))
+    );
+  }
+
+  @Test
+  public void testLocationField() throws NoSuchFieldException {
+    Class<ScheduleAppLocationEntity> clEntity = ScheduleAppLocationEntity.class;
+    Field field = clEntity.getDeclaredField("location");
+    assertNotNull(field.getAnnotation(Enumerated.class));
+  }
+
+}
