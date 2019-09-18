2276de9e417bf44c6a6ddbb8b0f80df77091a2c9
David Guzman <d.guzman@ucl.ac.uk>
2019-09-18 16:50:09 +0100

    Test jsonb conversion from response to object

---
+++ twitter-search-geo-ejb/src/test/java/uk/ac/ucl/twitter/search/geo/BearerTokenEntityTest.java
@@ -0,0 +1,19 @@
+package uk.ac.ucl.twitter.search.geo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import javax.json.bind.JsonbBuilder;
+
+public class BearerTokenEntityTest {
+
+  @Test
+  public void testFromJsonToObject() {
+    String jsonResponse = "{\"token_type\":\"bearer\",\"access_token\":\"AAAA%2FAAA%3DAAAAAAAA\"}";
+    BearerTokenEntity entity = JsonbBuilder.create()
+      .fromJson(jsonResponse, BearerTokenEntity.class);
+    Assertions.assertEquals("bearer", entity.getTokenType());
+    Assertions.assertEquals("AAAA%2FAAA%3DAAAAAAAA", entity.getAccessToken());
+  }
+
+}
