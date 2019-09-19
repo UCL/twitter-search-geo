0762bb798e867d9907f542b84934502bc081a98c
David Guzman <d.guzman@ucl.ac.uk>
2019-09-19 16:24:28 +0100

    WIP: change dependency from jersey client oauth2 to core. Build webtarget to call the search API [skip ci]

--- twitter-search-geo-ejb/src/main/java/uk/ac/ucl/twitter/search/geo/SearchClient.java
+++ twitter-search-geo-ejb/src/main/java/uk/ac/ucl/twitter/search/geo/SearchClient.java
@@ -1,40 +1,47 @@
 package uk.ac.ucl.twitter.search.geo;
 
-import org.glassfish.jersey.client.ClientConfig;
-import org.glassfish.jersey.client.oauth2.OAuth2ClientSupport;
-
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.inject.Named;
-import javax.ws.rs.core.Feature;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Named
 @Dependent
 public class SearchClient {
 
-  private static final String SEARCH_RESOURCE_URL =
-    "https://api.twitter.com/1.1/search/tweets.json";
+  private final String searchResourceHost = ClientConfiguration
+    .getFromSystemOrEnvOrElse(
+      "SEARCH_HOST",
+      "https://api.twitter.com"
+    );
 
-  /**
-   * Configurable for Jersey client.
-   */
-  private final ClientConfig clientConfig = new ClientConfig();
+  private static final String SEARCH_RESOURCE_PATH =  "/1.1/search/tweets.json";
+
+  private static final int NUMBER_OF_TWEETS = 100;
+
+  private final WebTarget webTarget = ClientBuilder.newClient()
+    .target(searchResourceHost)
+    .path(SEARCH_RESOURCE_PATH);
 
   @Inject
   private OAuth2Client oAuth2Client;
 
-  private final Feature feature = OAuth2ClientSupport.feature(
-    oAuth2Client.getBearerToken("")
-  );
+  @Inject
+  private EntityAccess entityAccess;
 
-  @PostConstruct
-  public void setUp() {
-    OAuth2ClientSupport.authorizationCodeGrantFlowBuilder(null, null, null);
-  }
-
-  public void search(Location location, String applicationName) {
-
+  public void search(final Location location, final String applicationName) {
+    final LocationEntity locationEntity = entityAccess.findLocationEntityByLocation(location);
+    final String sinceDate = LocalDate.now().format(DateTimeFormatter.ISO_DATE);
+    webTarget.queryParam("query", "since:" + sinceDate )
+      .queryParam("geocode", location.getLatitude() + "," + location.getLongitude() + "," + location.getRadius())
+      .queryParam("count", NUMBER_OF_TWEETS)
+      .queryParam("since_id", locationEntity.getSinceId())
+      .request(MediaType.APPLICATION_JSON_TYPE)
+      .header("Authorization", "Bearer " + oAuth2Client.getBearerToken(applicationName));
   }
 
 }
